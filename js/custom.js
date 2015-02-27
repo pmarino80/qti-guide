8711217c3629c60d77208a708094d65dfe65c05a
Paul Marino <paul.marino@waterfield.com>
2015-02-27 14:19:14 -0600

    added mail chimp

--- js/custom.js
+++ js/custom.js
@@ -22,7 +22,7 @@ $(".video-container").fitVids();
 
 $('.mailchimp').ajaxChimp({
     callback: mailchimpCallback,
-    url: "http://webdesign7.us6.list-manage.com/subscribe/post?u=9445a2e155b82208d73433060&amp;id=16dc80e353" //Replace this with your own mailchimp post URL. Don't remove the "". Just paste the url inside "".  
+    url: "//scoutfdc.us1.list-manage.com/subscribe/post?u=2caf5b1e03319446ad8c3e9e4&amp;id=9aa252b015" //Replace this with your own mailchimp post URL. Don't remove the "". Just paste the url inside "".  
 });
 
 function mailchimpCallback(resp) {
